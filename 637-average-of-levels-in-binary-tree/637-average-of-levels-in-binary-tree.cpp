4dbcd1de342f8e95074a6dc3f3fc3852211298c2
Pranshu <75555961+Pranshu820@users.noreply.github.com>
2022-09-03 02:08:45 +0530

    Time: 37 ms (12.17%), Space: 22.5 MB (72.97%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -1,29 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> ans;
+    void solve(TreeNode* root, vector<double>& ans){        
         queue<TreeNode*> q;
         q.push(root);
         
-        // while there are nodes remaining to be visited
         while(!q.empty()){
-            double temp = q.size();
-            double sum = 0;
-            // iterating each level of binary tree
-            for(int i = 0; i < temp; i++){
-                auto tt = q.front();
+            double size = q.size(), sum = 0;
+            
+            for(int i = 0; i < size; i++){
+                auto temp = q.front();
                 q.pop();
-                sum += (tt->val);
-				// check for not NULL and pushing into queue.
-                if(tt->left){
-                    q.push(tt->left);
-                }
-                if(tt->right){
-                    q.push(tt->right);
-                }
+                
+                sum += (temp -> val);
+                
+                if(temp -> left) q.push(temp -> left);
+                if(temp -> right) q.push(temp -> right);
             }
-            ans.push_back(sum/ temp);
+            ans.push_back(sum / size);
         }
+    }
+    
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        solve(root, ans);
         return ans;
     }
 };
