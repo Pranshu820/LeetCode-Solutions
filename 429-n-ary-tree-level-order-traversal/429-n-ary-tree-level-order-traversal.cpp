74bf95c6cc99a863eae65ecda19e876f965ad10f
Pranshu <75555961+Pranshu820@users.noreply.github.com>
2022-09-06 00:23:32 +0530

    Time: 49 ms (17.68%), Space: 11.6 MB (86.69%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if(!root)  return {};
+        vector<vector<int>> ans;
+        queue<Node*> q;
+        q.push(root); 
+        while(!q.empty()) {
+            int n = q.size(); 
+            vector<int> level;
+            for(int i = 0; i < n; i++){
+                Node* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                for(auto n : node->children) q.push(n); 
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
