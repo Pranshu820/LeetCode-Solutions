adeac951d12e748905413a293abc00cef63cab5c
Pranshu <75555961+Pranshu820@users.noreply.github.com>
2022-09-11 00:15:30 +0530

    Time: 4 ms (38.36%), Space: 7.5 MB (77.57%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -10,13 +10,9 @@ public:
             int stone2 = pq.top();
             pq.pop();
             
-            // if(stone1 > stone2) pq.push(stone1 - stone2);
-            // else if(stone1 < stone2) pq.push(stone2 - stone1);
-            
             pq.push(stone1 - stone2);
         }
         
-        // if(pq.empty()) return 0;
         return pq.top();
     }
 };
