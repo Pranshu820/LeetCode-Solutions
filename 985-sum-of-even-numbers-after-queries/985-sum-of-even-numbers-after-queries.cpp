04da056e2a64ff57ad0d3a776d29e3771a4756fb
Pranshu <75555961+Pranshu820@users.noreply.github.com>
2022-09-22 01:55:39 +0530

    Time: 264 ms (18.20%), Space: 46.1 MB (38.75%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -6,32 +6,13 @@ public:
         for(auto &i : nums) if(!(i & 1)) pre_sum += i;
         
         for(auto &i : queries){
-//             if(nums[i[1]] & 1){
-//                 nums[i[1]] += i[0];
-//                 if(!((nums[i[1]]) & 1)){
-//                     pre_sum += nums[i[1]];
-//                 }
-//                 ans.push_back(pre_sum);
-//             }
-            
-//             else{
-//                 pre_sum -= nums[i[1]];
-//                 nums[i[1]] += i[0];
-//                 if(!((nums[i[1]]) & 1)){
-//                     pre_sum += nums[i[1]];
-//                 }
-//                 ans.push_back(pre_sum);
-//             }
-            
-            
-            
             if(!(nums[i[1]] & 1)) pre_sum -= nums[i[1]];
+            
                 nums[i[1]] += i[0];
                 if(!((nums[i[1]]) & 1)){
                     pre_sum += nums[i[1]];
                 }
                 ans.push_back(pre_sum);
-            
         }
         
         return ans;
